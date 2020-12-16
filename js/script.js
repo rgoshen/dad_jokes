bc9930ef5b901adcec519e40a27aa22e31087da2
Richard Goshen <rick.goshen@gmail.com>
2020-12-15 17:56:58 -0700

    fetched dad jokes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// starter script
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeElement.innerHTML = data.joke;
+}
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
